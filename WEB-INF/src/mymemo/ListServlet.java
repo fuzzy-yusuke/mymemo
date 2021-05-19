a3fe7fa9363f8437bd7a31e739745460d8de5e1c
fuzzy-yusuke <youth_k0228@outlook.com>
2021-05-19 15:37:21 +0900

    web.xmlとjspファイル作成

--- WEB-INF/src/mymemo/ListServlet.java
+++ WEB-INF/src/mymemo/ListServlet.java
@@ -1,58 +1,22 @@
 package mymemo;
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
-import java.util.*;
-import java.sql.*;
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
 
 public class ListServlet extends HttpServlet {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException{
 
-        if (request.getAttribute("message") == null) {
-            request.setAttribute("message", "hello paiza memo");
-        }
-
-        String url = "jdbc:mysql://localhost/memo";
-        String user = "root";
-        String password = "";
-
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        String sql = "SELECT * FROM posts";
-        try (Connection connection = DriverManager.getConnection(url, user, password);
-             PreparedStatement statment = connection.prepareStatement(sql);
-             ResultSet results = statment.executeQuery()) {
-
-            ArrayList<HashMap<String, String>> rows = new ArrayList<HashMap<String, String>>();
-
-            while (results.next()) {
-                HashMap<String, String> columns = new HashMap<String, String>();
-
-                String id = results.getString("id");
-                columns.put("id", id);
-
-                String title = results.getString("title");
-                columns.put("title", title);
-
-                String content = results.getString("content");
-                columns.put("content", content);
-
-                rows.add(columns);
-            }
-
-            request.setAttribute("rows", rows);
-
-        } catch (Exception e) {
-            request.setAttribute("message", "Exception:" + e.getMessage());
-        }
-        String view = "/WEB-INF/views/list.jsp";
+    	request.setAttribute("message", "hello paiza memo");
+        String view = "/WEB-INF/view/list.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(view);
         dispatcher.forward(request, response);
-
     }
 }
