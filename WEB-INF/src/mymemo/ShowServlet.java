f7786f434cfb9161cf15c1e50ea535ff9b44219e
fuzzy-yusuke <youth_k0228@outlook.com>
2021-05-28 23:18:45 +0900

    新規Servletファイル作成

---
+++ WEB-INF/src/mymemo/ShowServlet.java
@@ -0,0 +1,67 @@
+package mymemo;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.HashMap;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class ShowServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException{
+    	
+    	int postId=Integer.parseInt(request.getParameter("id"));
+    	//dogetメソッドで受け取ったデータを「postId」変数に代入
+    	if(request.getAttribute("message")==null) {
+    		request.setAttribute("message", "This is your post" + postId);
+    	}
+    	
+    	String url = "jdbc:mysql://localhost/memo";
+        String user = "root";
+        String password = "";
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        String sql="SELECT * FROM posts WHERE id=?";
+        
+        try (	Connection connection = DriverManager.getConnection(url, user, password);
+                PreparedStatement statment = connection.prepareStatement(sql);
+                ) {
+
+               statment.setInt(1, postId); //DBから指定のデータを取り出す
+               ResultSet results = statment.executeQuery();
+
+               while (results.next()) {
+                   HashMap<String, String> columns = new HashMap<String, String>();
+
+                   String id = results.getString("id");
+                   columns.put("id", id);
+
+                   String title = results.getString("title");
+                   columns.put("title", title);
+
+                   String content = results.getString("content").replaceAll("\n","<br>" );
+                   request.setAttribute("content", content);
+               }
+
+           } catch (Exception e) {
+               request.setAttribute("message", "Exception:" + e.getMessage());
+           }
+    	
+    	String view = "/WEB-INF/view/post.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(view);
+        dispatcher.forward(request, response);
+    }
+}
